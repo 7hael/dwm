aeb1ac3755605461dd563a1721ff14aebe25ce1f
aeth_ <ae.thel@disroot.org>
2022-06-08 19:47:53 +0200

    bartabgroups

--- config.def.h
+++ config.def.h
@@ -17,9 +17,11 @@ static char selfgcolor[]            = "#eeeeee";
 static char selbordercolor[]        = "#005577";
 static char selbgcolor[]            = "#005577";
 static char *colors[][3] = {
-       /*               fg           bg           border   */
-       [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
-       [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
+       /*                      fg           bg           border   */
+       [SchemeNorm]        = { normfgcolor, normbgcolor, normbordercolor },
+       [SchemeSel]         = { selfgcolor,  selbgcolor,  selbordercolor  },
+	   [SchemeTabActive]   = { normfgcolor, normbgcolor, normbordercolor },
+	   [SchemeTabInactive] = { normfgcolor, normbgcolor, normbordercolor }
 };
 
 /* tagging */
@@ -50,6 +52,15 @@ static float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static int nmaster     = 2;    /* number of clients in master area */
 static int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 0; /* 1 will force focus on the fullscreen window */
+
+/* bartabgroups properties */
+#define BARTAB_BORDERS 1       // 0 = off, 1 = on
+#define BARTAB_BOTTOMBORDER 0  // 0 = off, 1 = on
+#define BARTAB_TAGSINDICATOR 1 // 0 = off, 1 = on if >1 client/view tag, 2 = always on
+#define BARTAB_TAGSPX 5        // # pixels for tag grid boxes
+#define BARTAB_TAGSROWS 3      // # rows in tag grid (9 tags, e.g. 3x3)
+static void (*bartabmonfns[])(Monitor *) = { monocle /* , customlayoutfn */ };
+static void (*bartabfloatfns[])(Monitor *) = { NULL /* , customlayoutfn */ };
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
